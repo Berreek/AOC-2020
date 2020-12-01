d76c0faceb4fa7166ff6ebc31aed88aa9e963c6b
Bartosz Behring <bartosz@spotio.com>
2020-12-01 11:32:38 +0100

    Day1

---
+++ Day1/Solution.fsx
@@ -0,0 +1,22 @@
+﻿open System.IO
+
+let values =
+    File.ReadAllLines "./Day1/Input.txt"
+    |> Seq.map int
+
+let printResult result = printfn $"Answer is {result}"
+
+let part1 input =
+    for value in input do
+        for innerValue in input do
+            if value + innerValue = 2020 then printResult (value * innerValue)
+
+let part2 input =
+    for value in input do
+        for innerValue in input do
+            for anotherInnerValue in input do
+                if value + innerValue + anotherInnerValue = 2020
+                then printResult (value * innerValue * anotherInnerValue)
+
+part1 values
+part2 values
