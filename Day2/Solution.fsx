a94464ba2d7cbdfac61729caf57255fdff4842c1
Bartosz Behring <bartosz@spotio.com>
2020-12-04 15:24:26 +0100

    Missing part 3

--- Day2/Solution.fsx
+++ Day2/Solution.fsx
@@ -1,32 +1,35 @@
-﻿open System.Linq;
+﻿open System.Linq
 open System.IO
 
-type PolicyPassword = {
-    SecondNumber: int
-    FirstNumber: int
-    Letter: char
-    Password: string
-}
+type PolicyPassword =
+    { SecondNumber: int
+      FirstNumber: int
+      Letter: char
+      Password: string }
 
 let getPolicies () =
     let map (line: string) =
         let splittedValues = line.Split ' '
         let policyInfo = splittedValues.[0].Split '-'
-        {
-            Password = splittedValues.[2]
-            Letter = splittedValues.[1].[0]
-            SecondNumber = int policyInfo.[1]
-            FirstNumber = int policyInfo.[0]
-        }
+
+        { Password = splittedValues.[2]
+          Letter = splittedValues.[1].[0]
+          SecondNumber = int policyInfo.[1]
+          FirstNumber = int policyInfo.[0] }
+
     File.ReadAllLines "./Day2/Input.txt"
     |> Seq.map map
-    
+
 let isPolicyValidForPart1 policy =
-    let charOccurrence = policy.Password.Count(fun x -> x = policy.Letter)
-    charOccurrence <= policy.SecondNumber && charOccurrence >= policy.FirstNumber
-    
+    let charOccurrence =
+        policy.Password.Count(fun x -> x = policy.Letter)
+
+    charOccurrence <= policy.SecondNumber
+    && charOccurrence >= policy.FirstNumber
+
 let isPolicyValidForPart2 policy =
-    match (policy.Password.[policy.FirstNumber - 1] = policy.Letter, policy.Password.[policy.SecondNumber - 1] = policy.Letter) with
+    match (policy.Password.[policy.FirstNumber - 1] = policy.Letter,
+           policy.Password.[policy.SecondNumber - 1] = policy.Letter) with
     | (true, false) -> true
     | (false, true) -> true
     | _ -> false
---
+++ Day3/Solution.fsx
@@ -0,0 +1,56 @@
+﻿open System.IO
+
+type Slope = { Right: int; Down: int }
+
+let everyNth n seq =
+    seq
+    |> Seq.mapi (fun i el -> el, i)
+    |> Seq.filter (fun (_, i) -> (i % n) = 0)
+    |> Seq.map fst
+
+let getInput () = File.ReadAllLines "./Day3/Input.txt"
+
+let getTreesCount (values: seq<string>) slope =
+    let maxLengthOfLine = (Seq.item 0 values).Length
+
+    let isATree slope index (item: Option<string>) =
+        match item with
+        | Some l -> l.[(index * slope.Right) % maxLengthOfLine] = '#'
+        | None -> false
+
+    let findATree slope lines =
+        let handleLine index _ =
+            let nextIndex = index + 1
+
+            lines
+            |> Seq.tryItem nextIndex
+            |> isATree slope nextIndex
+
+        lines |> Seq.mapi handleLine
+
+    values
+    |> everyNth slope.Down
+    |> findATree slope
+    |> Seq.filter id
+    |> Seq.length
+
+let input = getInput ()
+
+let part1 =
+    getTreesCount input { Right = 3; Down = 1 }
+
+printfn $"Result for part 1 is {part1}"
+
+let slopes =
+    [ { Right = 1; Down = 1 }
+      { Right = 3; Down = 1 }
+      { Right = 5; Down = 1 }
+      { Right = 7; Down = 1 }
+      { Right = 1; Down = 2 } ]
+
+let part2 =
+    slopes
+    |> Seq.map (getTreesCount input)
+    |> Seq.fold (fun init el -> init * el) 1
+
+printfn $"Result for part 2 is {part2}"
