f43885b328985fc64995b507c1c99a5c5a18c7ce
Bartosz Behring <bartosz@spotio.com>
2020-12-09 20:30:35 +0100

    Day 9

---
+++ Day9/Solution.fsx
@@ -0,0 +1,54 @@
+﻿open System.IO
+
+let input =
+    File.ReadAllLines "./Day9/Input.txt"
+    |> Seq.map int64
+    |> Seq.indexed
+
+let getInvalidNumber (index, valueToCheck) =
+    let isValueValid value1 value2 =
+        value1 <> value2 && value1 + value2 = valueToCheck
+
+    let valuesForVerification =
+        input
+        |> Seq.skip (index - 25)
+        |> Seq.take 25
+        |> Seq.map snd
+
+    valuesForVerification
+    |> Seq.exists (fun value ->
+        valuesForVerification
+        |> Seq.exists (isValueValid value))
+    |> not
+
+let getRangeSum valueToCheck =
+    let rangeValues =
+        input
+        |> Seq.map (fun (index, _) ->
+            input
+            |> Seq.map snd
+            |> Seq.skip index
+            |> Seq.scan (fun (cumulated, _) value -> (cumulated + value, value)) (int64 0, int64 0)
+            |> Seq.takeWhile (fun (cumulated, _) -> cumulated <= valueToCheck)
+            |> Seq.map snd
+            |> Seq.skip 1)
+        |> Seq.find (fun range -> range |> Seq.sum |> (=) valueToCheck)
+
+    Seq.min rangeValues + Seq.max rangeValues
+
+let part1 =
+    input
+    |> Seq.skip 25
+    |> Seq.find getInvalidNumber
+    |> snd
+
+printfn $"Answer for part 1 is {part1}"
+
+let part2 =
+    input
+    |> Seq.skip 25
+    |> Seq.find getInvalidNumber
+    |> snd
+    |> getRangeSum
+
+printfn $"Answer for part 2 is {part2}"
